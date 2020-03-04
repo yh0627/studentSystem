a084d9755b146aa953a70d09052c108ffcbc9678
yh0627 <2904085216@qq.com>
2020-03-04 13:07:50 +0800

    豆瓣电影

---
+++ js/ajax.js
@@ -0,0 +1,15 @@
+//接口的前缀
+let devUrl = `https://douban-api-git-master.zce.now.sh`;
+let testUrl = `https://douban-api.uiee.com`;
+let proUrl = `https://douban-api.now.sh`;
+//当前使用的环境
+let proUrl = proUrl;
+
+//正在上映
+let in_theaters = `${baseUrl}/v2/movie/in_theaters`;
+
+//即将上映
+let in_theaters = `${baseUrl}/v2/movie/coming_soon`;
+
+//新片榜
+let in_theaters = `${baseUrl}/v2/movie/new_movies`;
